6a18ffb30fbd4182cf98ab682d95caf44602628e
Lisandro Dalcin <dalcinl@gmail.com>
2024-02-12 20:50:43 +0300

    Enhancements to check wheel script

--- check-wheel.sh
+++ check-wheel.sh
@@ -25,52 +25,60 @@ data=$(ls -d "$pkgname"-*.data/data)
 if test "$(uname)" = Linux; then
     # shellcheck disable=SC2016
     runpath='\$ORIGIN/../lib|\$ORIGIN'
-    runlibs='lib(c|m|dl|rt|pthread)|ld-linux-.*\.so|ld64\.so'
-    print-rpath() { patchelf --print-rpath "$1"; }
-    print-needed() { patchelf --print-needed "$1"; }
+    runlibs='lib(mpi|c|m|dl|rt|pthread)\.so\..*'
+    runlibs=$runlibs'|(ld-linux-.*|ld64)\.so\..*'
+    print-runpath() { patchelf --print-rpath  "$1"; }
+    print-needed()  { patchelf --print-needed "$1"; }
     if test -f "$data"/lib/libucp.so; then
         runlibs=$runlibs'|libuc(m|p|s|t)'
     fi
 fi
 if test "$(uname)" = Darwin; then
     runpath='@executable_path/../lib/|@loader_path/'
-    runlibs='lib(mpi|pmpi|System)'
-    print-rpath()  { otool -l "$1" | sed -n '/RPATH/{n;n;p;}'; }
-    print-needed() { otool -L "$1" | sed 1,1d; }
+    runlibs='lib(mpi|pmpi|System)\..*\.dylib'
+    print-runpath() { otool -l "$1" | sed -n '/RPATH/{n;n;p;}'; }
+    print-needed()  { otool -L "$1" | sed 1,1d; }
     if test -f "$data"/lib/libucp.dylib; then
-        runlibs=$runlibs'|libuc(m|p|s|t)'
+        runlibs=$runlibs'|libuc(m|p|s|t)\.'
     fi
 fi
 
+check-dso() {
+    local dso=$1 out1="" out2=""
+    echo checking "$dso"...
+    test -f "$dso" || printf "ERROR: file not found"
+    out1="$(print-runpath "$dso" | grep -vE "$runpath" || true)"
+    test -z "$out1" || printf "ERROR: RUNPATH\n%s\n" "$out1"
+    out2="$(print-needed  "$dso" | grep -vE "$runlibs" || true)"
+    test -z "$out2" || printf "ERROR: NEEDED\n%s\n" "$out2"
+    test -z "$out1" -a  -z "$out2"
+}
+
 for hdr in "$data"/include/mpi.h "$data"/include/mpio.h; do
-    echo check "$hdr"...
+    echo checking "$hdr"...
     test -f "$hdr"
+    test -z "$(grep -E '^#include\s+"mpicxx\.h"' "$hdr" || true)"
 done
 for script in "$data"/bin/mpicc "$data"/bin/mpicxx; do
-    echo check "$script"...
-    test -z "$(grep -E "/opt/$mpiname" "$script")"
+    echo checking "$script"...
+    test -f "$script"
+    test -z "$(grep -E "/opt/$mpiname" "$script" || true)"
 done
 for bin in "$data"/bin/mpichversion "$data"/bin/mpivars; do
-    echo check "$bin"...
-    test -z "$(print-rpath  "$bin" | grep -vE "$runpath")"
-    test -z "$(print-needed "$bin" | grep -vE "$runlibs")"
+    check-dso "$bin"
 done
 for bin in "$data"/bin/mpiexec* "$data"/bin/hydra_*; do
-    echo check "$bin"...
-    test -z "$(print-rpath  "$bin" | grep -vE "$runpath")"
-    test -z "$(print-needed "$bin" | grep -vE "$runlibs")"
+    check-dso "$bin"
 done
 for lib in "$data"/lib/libmpi.*; do
-    echo check "$lib"...
-    test -z "$(print-rpath  "$lib" | grep -vE "$runpath")"
-    test -z "$(print-needed "$lib" | grep -vE "$runlibs")"
+    check-dso "$lib"
 done
 if test "$(uname)" = Linux; then
     libs=$(ls -d "$pkgname".libs)
-    echo check "$libs"...
+    echo checking "$libs"...
     test -z "$(ls -A "$libs")"
 fi
 
-echo success
+echo success!
 
 done
